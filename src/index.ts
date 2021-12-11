161c63c684c29368592912ba6dcd76803c3eedfb
Jimmy WENG <jimmy.weng@efrei.net>
2021-12-11 20:49:42 +0100

    feat: add logging in app starter

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,15 @@ import dotenv from 'dotenv';
 
 import { app } from './app';
 import { getEnv } from './utils/getEnv';
+import { getLogger } from './utils/logging/getLogger';
 
 dotenv.config({ path: `/.env.${process.env.NODE_ENV?.toLowerCase() ?? ''}` });
 dotenv.config();
 
+/**
+ * main logger
+ */
+const LOGGER = getLogger('E-Calendar');
+
 const PORT = parseInt(getEnv('PORT', '8080'));
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => LOGGER.info(`Server is running on port ${PORT}`));
