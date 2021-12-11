633b74b6f8f6641880df9e3a79519147947fb6c3
Jimmy WENG <jimmy.weng@efrei.net>
2021-12-11 21:33:27 +0100

    chore: refactor

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,6 @@ import { getLogger } from './utils/logging/getLogger';
 dotenv.config({ path: `/.env.${process.env.NODE_ENV?.toLowerCase() ?? ''}` });
 dotenv.config();
 
-/**
- * main logger
- */
 const LOGGER = getLogger('E-Calendar');
-
 const PORT = parseInt(getEnv('PORT', '8080'));
 app.listen(PORT, () => LOGGER.info(`Server is running on port ${PORT}`));
--- src/utils/logging/registerRequestLogger.ts
+++ src/utils/logging/registerRequestLogger.ts
@@ -1,9 +1,10 @@
 /* istanbul ignore file */
 import { Express, Request } from 'express';
-import morgan, { StreamOptions } from 'morgan';
+import morgan from 'morgan';
 
 import { getLogger } from './getLogger';
-import { ILogger } from './interfaces/ILogger';
+
+const LOGGER = getLogger('REQUEST');
 
 morgan.token<Request>('parameters', (req) => {
 	const parameters = {
@@ -16,9 +17,9 @@ morgan.token<Request>('parameters', (req) => {
 
 const skipTests = (): boolean => process.env.NODE_ENV === 'test';
 
-const getStream = (logger: ILogger): StreamOptions => ({
-	write: (message: string): void => logger.info(message.slice(0, -1))
-});
+const stream = {
+	write: (message: string): void => LOGGER.info(message.slice(0, -1))
+};
 
 /**
  * register the request logger
@@ -26,13 +27,11 @@ const getStream = (logger: ILogger): StreamOptions => ({
  * @param app express application
  */
 export const registerRequestLogger = (app: Express): void => {
-	const LOGGER = getLogger('REQUEST');
-
 	app.use(
 		morgan('## START ## [:remote-addr] :method :url\n:parameters', {
 			immediate: true,
 			skip: skipTests,
-			stream: getStream(LOGGER)
+			stream
 		})
 	);
 	app.use(
@@ -40,7 +39,7 @@ export const registerRequestLogger = (app: Express): void => {
 			'## END ## [:remote-addr] :status - :method :url - :response-time ms',
 			{
 				skip: skipTests,
-				stream: getStream(LOGGER)
+				stream
 			}
 		)
 	);
