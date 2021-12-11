366edad1aaf8b8c285d8a8c08938a3bc6ec57a61
Jimmy WENG <jimmy.weng@efrei.net>
2021-12-11 19:15:54 +0100

    chore: add types

---
+++ src/utils/logging/ILogger.ts
@@ -0,0 +1,15 @@
+import { LoggingLevelEnum } from './types';
+
+export interface ILogger {
+	log(level: LoggingLevelEnum, message: string, err?: Error): void;
+
+	debug(message: string, err?: Error): void;
+
+	info(message: string): void;
+
+	warn(message: string, err?: Error): void;
+
+	error(message: string, err?: Error): void;
+
+	fatal(message: string, err?: Error): void;
+}
