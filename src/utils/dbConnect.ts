4e9f49860fe8a21241e756a9481727cf809fea60
Weng Jimmy <jimmy.weng@efrei.net>
2022-01-04 15:36:59 +0100

    fix: db connection

--- src/utils/dbConnect.ts
+++ src/utils/dbConnect.ts
@@ -10,16 +10,16 @@ const LOGGER = getLogger('DATABASE');
  * connect to the database
  */
 export const dbConnect = (): void => {
-	const username = getEnv('MONGODB_USERNAME');
-	const password = getEnv('MONGODB_PASSWORD');
+	const user = getEnv('MONGODB_USERNAME');
+	const pass = getEnv('MONGODB_PASSWORD');
 	const host = getEnv('MONGODB_HOST');
 	const port = getEnv('MONGODB_PORT');
-	const database = getEnv('MONGODB_DATABASE');
+	const dbName = getEnv('MONGODB_DATABASE');
 
-	const uri = `mongodb://${username}:${password}@${host}:${port}/${database}`;
+	const uri = `mongodb://${host}:${port}`;
 
 	mongoose
-		.connect(uri)
+		.connect(uri, { user, pass, dbName })
 		.catch((err) =>
 			LOGGER.fatal(
 				"Can't connect to the database.",
