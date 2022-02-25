c3a4283b82ee0d423e0f2a4372d8b1c819221733
Jimmy WENG <jimmy.weng@efrei.net>
2022-02-25 13:33:01 +0100

    feat: add tests setup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import 'reflect-metadata';
 
-import dotenv from 'dotenv';
+import './setupEnv';
 
 import { app } from './app';
 import { dbConnect } from './utils/db/dbConnect';
 import { getEnv } from './utils/getEnv';
 import { getLogger } from './utils/logging/getLogger';
 
-dotenv.config({ path: `/.env.${process.env.NODE_ENV?.toLowerCase() ?? ''}` });
-dotenv.config();
-
-dbConnect();
-
 const LOGGER = getLogger('E-Calendar');
-const PORT = parseInt(getEnv('PORT', '8080'));
-app.listen(PORT, () => LOGGER.info(`Server is running on port ${PORT}`));
+
+dbConnect()
+	.then(() => {
+		const PORT = parseInt(getEnv('PORT', '8080'));
+		app.listen(PORT, () =>
+			LOGGER.info(`Server is running on port ${PORT}`)
+		);
+	})
+	.catch((err) => {
+		LOGGER.fatal(
+			'Application could not start correctly',
+			err instanceof Error
+				? err
+				: new Error('Application could not start correctly')
+		);
+	});
--- src/setupTest.ts
+++ src/setupTest.ts
@@ -1,14 +1,21 @@
 import 'reflect-metadata';
 
+import './setupEnv';
+
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import dirtyChai from 'dirty-chai';
 import supertest from 'supertest';
 
 import { app } from './app';
+import { dbConnect } from './utils/db/dbConnect';
+import { getLogger } from './utils/logging/getLogger';
+import { dropDatabase } from './utils/test/dbUtils';
 
 chai.use(chaiAsPromised);
 chai.use(dirtyChai);
+
+const LOGGER = getLogger('setupTest');
 
 /**
  * chai's should command
@@ -19,3 +26,28 @@ export const itShould = chai.should();
  * prepared supertest request
  */
 export const appRequest = supertest(app);
+
+export const mochaHooks = {
+	/**
+	 * hook to execute at each test
+	 *
+	 * @param done callback to indicate hook end
+	 */
+	beforeEach(done: () => void): void {
+		dbConnect()
+			.then(() => {
+				return dropDatabase();
+			})
+			.then(() => {
+				done();
+			})
+			.catch((err) => {
+				LOGGER.error(
+					'Error during before each hook',
+					err instanceof Error
+						? err
+						: new Error('Error during before each hook')
+				);
+			});
+	}
+};
